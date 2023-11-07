e4ca8bcd05d0ffd92cb75051405cac6545d5f6f6
dduartevla <deboraduarte@ice.ufjf.br>
2023-11-07 17:09:25 -0300

    Cria classe Feijao

--- src/main/java/Feijao.java
+++ src/main/java/Feijao.java
@@ -1,2 +1,42 @@
+import java.util.Date;
+
 public class Feijao {
+
+    private String dataPlantio;
+    private double areaCultivada;
+    private String intervencaoSugerida;
+
+    public Feijao(String dataPlantio, double areaCultivada, String intervencaoSugerida){
+        this.dataPlantio = dataPlantio;
+        this.areaCultivada = areaCultivada;
+        this.intervencaoSugerida = intervencaoSugerida;
+    }
+
+    public String getDataPlantio() {
+        return dataPlantio;
+    }
+
+    public void setDataPlantio(String dataPlantio) {
+        this.dataPlantio = dataPlantio;
+    }
+
+    public double getAreaCultivada() {
+        return areaCultivada;
+    }
+
+    public void setAreaCultivada(double areaCultivada) {
+        this.areaCultivada = areaCultivada;
+    }
+
+    public String getIntervencaoSugerida() {
+        return intervencaoSugerida;
+    }
+
+    public void setIntervencaoSugerida(String intervencaoSugerida) {
+        this.intervencaoSugerida = intervencaoSugerida;
+    }
+
+    public String aceitar(Visitor visitor) {
+        return visitor.exibirFeijao(this);
+    }
 }
